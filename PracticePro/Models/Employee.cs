e7b02b0c49fdba78f79b0949631946c54df3a677
shetkart <shetkarthik89@gmail.com>
2022-10-12 15:13:22 +0530

    Add project files.

---
+++ PracticePro/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticePro.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string? Pname { get; set; }
+    }
+}
---
+++ PracticePro/Models/new_dbContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PracticePro.Models
+{
+    public partial class new_dbContext : DbContext
+    {
+        public new_dbContext()
+        {
+        }
+
+        public new_dbContext(DbContextOptions<new_dbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Worker> Workers { get; set; } = null!;
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=ELW5133\\SQLEXPRESS;Database=new_db;Trusted_Connection=True;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Pname)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("pname");
+            });
+
+            modelBuilder.Entity<Worker>(entity =>
+            {
+                entity.ToTable("Worker");
+
+                entity.Property(e => e.WorkerId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("WORKER_ID");
+
+                entity.Property(e => e.Department)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("DEPARTMENT")
+                    .IsFixedLength();
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("FIRST_NAME")
+                    .IsFixedLength();
+
+                entity.Property(e => e.JoiningDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("JOINING_DATE");
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("LAST_NAME")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Salary).HasColumnName("SALARY");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
